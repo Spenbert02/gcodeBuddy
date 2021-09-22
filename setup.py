83467a8ce1743afd0de7a36f0df029cdccbf31ea
Spenbert02 <spenbert02@gmail.com>
2021-09-21 23:34:43 -0500

    fixed misnamed files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="gcodeBuddy",
-    version="0.2.0",
+    version="0.2.1",
     description="Python package used to write to, read from, and interpret g-code files",
     long_description=long_description,
     long_description_content_type="text/markdown",
